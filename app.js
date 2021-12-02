75890e7e3592717d5c126f2e7e68da1eeb3ebde6
Nikolay Yaryhin <nik.oroferov@gmail.com>
2021-12-02 20:55:45 +0200

    fix .env

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const { DB_HOST } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connect"))
+  .catch(error => console.log(error.message));
+
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();

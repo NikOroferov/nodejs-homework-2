6c9214608a127bba22a05832f026d9e983bb74d2
Nikolay Yaryhin <nik.oroferov@gmail.com>
2021-12-04 23:58:52 +0200

    fix errors

---
+++ models/contact.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const joiContactSchema = Joi.object({
+  name: Joi.string().required(),
+  phone: Joi.string().required(),
+  email: Joi.string().required(),
+  favorite: Joi.bool(),
+});
+
+const joiFavoriteSchema = Joi.object({
+  favorite: Joi.bool().default(false).required(),
+});
+
+const contactSchema = Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Contact = model("contact", contactSchema);
+
+module.exports = {
+  joiContactSchema,
+  joiFavoriteSchema,
+  Contact,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const createError = require("http-errors");
-const Joi = require("joi");
-
-const addContactSchema = Joi.object({
-  name: Joi.string().required(),
-  phone: Joi.string().required(),
-  email: Joi.string().required(),
-});
-
-const contactsOperation = require("../../model/index.js");
+const {
+  joiContactSchema,
+  joiFavoriteSchema,
+  Contact,
+} = require("../../models/contact");
 
 router.get("/", async (req, res, next) => {
   try {
-    const contacts = await contactsOperation.listContacts();
+    const contacts = await Contact.find({});
     res.json({
       status: "success",
       code: 200,
@@ -29,9 +25,9 @@ router.get("/", async (req, res, next) => {
 router.get("/:contactId", async (req, res, next) => {
   try {
     const { contactId } = req.params;
-    const result = await contactsOperation.getContactById(contactId);
+    const result = await Contact.findById(contactId);
     if (!result) {
-      throw createError(404, `Product with id=${contactId} not found`);
+      throw createError(404, `Contact with id=${contactId} not found`);
       return;
     }
     res.json({
@@ -48,12 +44,12 @@ router.get("/:contactId", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
-    const { error } = addContactSchema.validate(req.body);
+    const { error } = joiContactSchema.validate(req.body);
     if (error) {
       error.status = 400;
       next(error);
     }
-    const result = await contactsOperation.addContact(req.body);
+    const result = await Contact.create(req.body);
     res.status(201).json({
       status: "success",
       code: 201,
@@ -69,9 +65,9 @@ router.post("/", async (req, res, next) => {
 router.delete("/:contactId", async (req, res, next) => {
   try {
     const { contactId } = req.params;
-    const result = await contactsOperation.removeContact(contactId);
+    const result = await Contact.findByIdAndRemove(contactId);
     if (!result) {
-      throw createError(404, `Product with id=${contactId} not found`);
+      throw createError(404, `Contact with id=${contactId} not found`);
       return;
     }
     res.json({
@@ -89,21 +85,52 @@ router.delete("/:contactId", async (req, res, next) => {
 
 router.put("/:contactId", async (req, res, next) => {
   try {
-    const { error } = addContactSchema.validate(req.body);
+    const { error } = joiContactSchema.validate(req.body);
     if (error) {
       error.status = 400;
       throw error;
     }
     const { contactId } = req.params;
-    const result = await contactsOperation.updateContact(contactId, req.body);
+    const result = await Contact.findByIdAndUpdate(contactId, req.body);
     if (!result) {
-      throw createError(404, `Product with id=${contactId} not found`);
+      throw createError(404, `Contact with id=${contactId} not found`);
       return;
     }
     res.json({
       message: "contact updated",
       status: "success",
       code: 200,
+      data: {
+        result,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.patch("/:contactId/favorite", async (req, res, next) => {
+  try {
+    const { error } = joiFavoriteSchema.validate(req.body);
+    if (error) {
+      error.status = 400;
+      error.message = "missing field favorite";
+      throw error;
+    }
+    const { contactId } = req.params;
+    const { favorite } = req.body;
+    const result = await Contact.findByIdAndUpdate(
+      contactId,
+      { favorite },
+      { new: true }
+    );
+    if (!result) {
+      throw createError(`Contact with id=${contactId} not found`);
+    }
+    res.json({
+      status: "success",
+      code: 200,
+      message: "contact update",
       data: {
         result,
       },
